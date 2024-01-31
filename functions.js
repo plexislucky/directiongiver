25d189176d575147b92af9b51065c6be8d19f84a
plex <109311001+plexislucky@users.noreply.github.com>
2024-01-31 22:10:29 +0200

    Initial commit
    Added all site files.

---
+++ functions.js
@@ -0,0 +1,20 @@
+// neciešami debīls kods, vajag labāku kodu lkm
+function DisplayImage(roomNumber) {
+
+    const prevImg = document.getElementById("roomImage")
+
+    if (prevImg != null) {
+        prevImg.remove()
+    }
+
+    var img = document.createElement("img");
+    img.src = "res/" + roomNumber + ".jpg"
+    img.id = "roomImage"
+    img.style = "display: block; margin-left: auto; margin-right: auto; width: 50%;"
+
+    console.log(roomNumber + ".jpg")
+
+    document.body.appendChild(img)
+}
+
+
